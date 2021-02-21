ccf5633326c2f90a80d619d8464f67865d229f28
Lukasz Malewski <lukasz.malewski@infobip.com>
2021-02-21 22:17:38 +0100

    refactoring

--- src/main/kotlin/com/malfoj/rambo/interceptor/WebConfig.kt
+++ src/main/kotlin/com/malfoj/rambo/interceptor/WebConfig.kt
@@ -1,8 +1,8 @@
 package com.malfoj.rambo.interceptor
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
